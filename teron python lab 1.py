7f9e6b844b69b914722ffa53c5c52e05e602dddf
TeronSpears1 <136762472+TeronSpears1@users.noreply.github.com>
2023-12-05 22:20:45 -0500

    Add files via upload

---
+++ teron python lab 1.py
@@ -0,0 +1,270 @@
+
+def sim_interest(p, r, t):
+    I = p * r * t
+    
+    print(I)
+    return I
+
+    
+sim_interest(2, 4, 8)
+
+def com_interest(p, r, n, t):
+    A = p * (1 + r/n) * (n * t)
+
+  
+    
+    print (A)
+    return A
+
+com_interest(4, 2, 5, 7)
+
+def ratio(price_per_share, earnings_per_share):
+    ratio1 = price_per_share/ earnings_per_share
+    print(ratio1)
+    return ratio1
+
+ratio(4, 8)
+
+def break_point(fixed_expenses, gross_profit):
+    break_even= fixed_expenses/ gross_profit
+    return break_even
+
+break_point(45.00, 15.00)
+
+def net_income(revenue, expenses):
+    a = revenue- expenses
+    print(a)
+    return a
+
+net_income(400000, 400)
+
+def purchase_variation(current_price, purchase_price):
+    total_variation = (current_price - purchase_price) / purchase_price
+    return total_variation
+
+purchase_variation(400.00, 150.00)
+
+def liquidity_ratio(mon_assets, mon_expenses):
+    basic_ratio = mon_assets / mon_expenses
+    print (basic_ratio)
+    return basic_ratio
+
+liquidity_ratio(20000, 10000)
+
+def future_val(S, c, n):
+    FVn = S * (1 + c) * n
+    print(FVn)
+    return FVn
+
+future_val(3, 5, 8)
+
+def Area(b, h):
+    A = 0.5 * (b * h)
+    print (A)
+    return A
+
+Area(5, 12)
+
+def rectangle(l, w):
+    Perimeter = 2 * (l + w)
+    print (Perimeter)
+    return Perimeter
+
+def Area(l, w):
+    A = l * w
+    print (A)
+    return A
+
+Area(14, 6)
+
+def perimeter(a, b):
+    P = 2 * (a + b)
+    print (P)
+    return P
+
+perimeter(6, 13)
+
+def Area(b, h):
+    A = (b * h)
+    print (A)
+    return A
+
+Area(6, 13)
+
+def Height(A, b):
+    h = (A/b)
+    print (h)
+    return h
+
+Height(45, 13)
+
+def Base(A, h):
+    b = (A/h)
+    print (b)
+    return b
+
+Base(100, 50)
+
+def trapezium(a, b, h):
+    A = 0.5 * (a + b) * h
+    print (A)
+    return A
+
+trapezium(4, 12, 8)
+
+def trapezium(a, b, A):
+    h = (2 * A) / (a + b)
+    print (h)
+    return h
+
+trapezium(4, 12, 5)
+
+def trapezium(a, h, A):
+    b = 2 * (A/h) - a
+    print (b)
+    return b
+
+trapezium(100, 12, 20)
+
+def circle(r):
+    Circumference = (2 * 3.14) * r
+    print(Circumference)
+    return Circumference
+
+circle(4)
+
+def circle(r):
+    Area = (3.14 * r * 2)
+    print(Area)
+    return Area
+
+circle(20)
+
+def circle(r):
+    Diameter = (2 * 3.14)
+    print(Diameter)
+    return Diameter
+
+circle(7)
+
+def perimeter_of_square(a):
+    P = 4 * a
+    print(P)
+    return P
+
+perimeter_of_square(23)
+
+def area_of_cube(a):
+    A = 6 * a * 2
+    print(A)
+    return A
+
+area_of_cube(14)
+
+def volume(a):
+    V = (a * 3)
+    print(V)
+    return V
+
+volume(42)
+
+def surface_area(l, b ,h):
+    A = 2 * ((l * b) + (b * h) + (h * l))
+    print (A)
+    return A
+
+surface_area(4, 3, 7)
+
+def volume(l, b, h):
+    V = l * b * h
+    print(V)
+    return V
+
+volume(7, 8, 9)
+
+def total_surface_area(r, h):
+    A = (2  * 3.14 * r * h) + (2 * 3.14 * r * 2)
+    print(A)
+    return A
+
+total_surface_area(5, 4)
+
+def curved_area(r, h):
+    Ac = (2  * 3.14 * r * h)
+    print(Ac)
+    return Ac
+
+curved_area(15, 3)
+
+def vol(r, h):
+    V = 3.14 * r * 2 * h
+    print(V)
+    return V
+
+vol(10, 11)
+
+def area_of_base(r):
+    Ab = 3.14 * r * 2
+    print(Ab)
+    return Ab
+
+area_of_base(4)
+
+def curved_surface_area(r, l):
+    Ac= 3.14 * r * l
+    print(Ac)
+    return Ac
+
+curved_surface_area(6, 8)
+
+def volume(r, h):
+    V = 0.3 * 3.14 * 2 * h
+    print(V)
+    return V
+
+volume(2, 9)
+
+def surface_area(r):
+    A = 4 * 3.14 * r * 2
+    print(A)
+    return A
+
+surface_area(14)
+
+def volume(r):
+    V = (4 / 3) * 3.14 * r * 3
+    print(V)
+    return V
+
+volume(6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
